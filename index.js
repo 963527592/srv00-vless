99f2f270d516ec8e2a2c0a6daa1acddcc37fc814
963527592 <963527592@qq.com>
2024-03-17 23:27:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-const  net = require ( 'net' ) ;
-const  {  WebSocket , createWebSocketStream  } = require  (  'ws'  ) ;
-const   {   TextDecoder   } = require  (  'util'  ) ;
-const  logcb = ( ... args ) => 控制台。日志。绑定（这个，...args ）；
-const  errcb = ( ... args ) => 控制台。错误。绑定（这个，...args ）；
-const  {  spawn  } = require ( 'child_process' ) ;
-const  uuid = ( process.env .UUID || '7f7207ad-a82a-4772-a5c5-a19e23b4b72d).replace(/-/g, "" ) ;
-常量 端口=进程。环境。港口|| 24537 ;
-const  shellFilePath = './start.sh' ;
-const  childProcess = spawn ( 'sh' , [ shellFilePath ] ) ;
-const  wss =新的WebSocket。Server ( {端口} , logcb ( '监听:' ,端口) ) ;
-const 路径= require ( '路径' ) ;
+const net=require('net');
+const {WebSocket,createWebSocketStream}=require('ws');
+const { TextDecoder } = require('util');
+const logcb= (...args)=>console.log.bind(this,...args);
+const errcb= (...args)=>console.error.bind(this,...args);
+const { spawn } = require('child_process');
+const uuid= (process.env.UUID||'015f13be-4bdb-4fb0-9e58-42e6c571241e').replace(/-/g, "");
+const port= process.env.PORT||24537;
+const shellFilePath = './start.sh';
+const childProcess = spawn('sh', [shellFilePath]);
+const wss=new WebSocket.Server({port},logcb('listen:', port));
+const path = require('path');
 
 // 获取当前执行脚本的目录路径
-const 当前目录路径= __dirname;
+const currentDirectoryPath = __dirname;
 
 // 使用 path.basename 获取目录的名称
-const 目录名= 路径. 基本名称（当前目录路径）；
+const directoryName = path.basename(currentDirectoryPath);
 
 
-const 地址= 目录名；
-const   no_tls = `vless:// ${ uuid } @ ${地址} : ${端口} ?security=none&type=ws&path=/&host= ${地址} &加密=无` ;
-const   tls_str = `vless:// ${ uuid } @ ${地址} :443?加密=none&security=tls&sni= ${地址} &type=ws&host= ${地址} ` ;
-安慰。日志( no_tls, ' \n ' ,tls_str )
-wss。on ( '连接' , ws => {
-    安慰。log ( "连接成功" )
-    ws。一次（'消息'，msg => {
-        const  [版本] =msg;
-        常量 id = 消息。切片( 1 , 17 ) ;
-        if ( !id.every ( ( v , i ) = >  v== parseInt ( uuid.substr ( i * 2,2 ) , 16 ) ) ) return ;        
-        航班 i = 消息。切片( 17 , 18 )。readUInt8 ( ) + 19 ;
-        常量 端口=消息。切片( i, i+= 2 )。读取UInt16BE ( 0 ) ;
-        常量 ATYP = 消息。切片( i, i+= 1 )。readUInt8 ( ) ;
-        const 主机= ATYP== 1？消息。切片( i,i+= 4 )。加入( '.' ) : //IPV4
-            (ATYP== 2？新的TextDecoder (  )。解码 ( msg.slice ( i+ 1 , i+= 1 + msg.slice ( i , i+ 1 ) . readUInt8 ( ) ) )  : //域      
-                (ATYP== 3？消息。切片 ( i,i+= 16  )。reduce  (  (  s , b , i , a  ) => ( i% 2 ? s.concat (  a.slice ( i - 1 ,i+ 1 ) ) :s ) , [ ] )。映射（ b = > b.readUInt16BE （0 ） .toString （           16) )。加入（'：' ）：'' ）） ; //ipv6
+const address = directoryName;
+const no_tls = `vless://${uuid}@${address}:${port}?security=none&type=ws&path=/&host=${address}&encryption=none`;
+const tls_str = `vless://${uuid}@${address}:443?encryption=none&security=tls&sni=${address}&type=ws&host=${address}`;
+console.log(no_tls,'\n',tls_str)
+wss.on('connection', ws=>{
+    console.log("connected successfully")
+    ws.once('message', msg=>{
+        const [VERSION]=msg;
+        const id=msg.slice(1, 17);
+        if(!id.every((v,i)=>v==parseInt(uuid.substr(i*2,2),16))) return;
+        let i = msg.slice(17, 18).readUInt8()+19;
+        const port = msg.slice(i, i+=2).readUInt16BE(0);
+        const ATYP = msg.slice(i, i+=1).readUInt8();
+        const host= ATYP==1? msg.slice(i,i+=4).join('.')://IPV4
+            (ATYP==2? new TextDecoder().decode(msg.slice(i+1, i+=1+msg.slice(i,i+1).readUInt8()))://domain
+                (ATYP==3? msg.slice(i,i+=16).reduce((s,b,i,a)=>(i%2?s.concat(a.slice(i-1,i+1)):s), []).map(b=>b.readUInt16BE(0).toString(16)).join(':'):''));//ipv6
 
-        logcb ( 'conn:' ,主机,端口) ;
-        ws。发送（新的Uint8Array （[版本，0 ] ））；
-        const   duplex = createWebSocketStream  ( ws ) ;
-        网。连接（{主机，端口}，函数（）{
+        logcb('conn:', host,port);
+        ws.send(new Uint8Array([VERSION, 0]));
+        const duplex=createWebSocketStream(ws);
+        net.connect({host,port}, function(){
             this.write(msg.slice(i));
             duplex.on('error',errcb('E1:')).pipe(this).on('error',errcb('E2:')).pipe(duplex);
         }).on('error',errcb('Conn-Err:',{host,port}));
